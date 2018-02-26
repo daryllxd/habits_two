b0657d203027a33fefe6e97790d0fe7e056ae309
Daryll Santos <daryll.santos@gmail.com>
2018-02-26 13:27:56 +0800

    Added flash message view helper.

--- lib/habits_two_web/controllers/page_controller.ex
+++ lib/habits_two_web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule HabitsTwoWeb.PageController do
   use HabitsTwoWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> render "index.html"
   end
 end
--- lib/habits_two_web/views/layout_view.ex
+++ lib/habits_two_web/views/layout_view.ex
@@ -1,3 +1,22 @@
 defmodule HabitsTwoWeb.LayoutView do
   use HabitsTwoWeb, :view
+
+  def show_notification(conn, type, message) do
+    conn
+    |> flash_message(%{"#{type}" => message})
+  end
+
+  def flash_message(conn, %{"info" => message}) do
+    if message do
+      render "flash.html", class: "alert-primary", message: message
+    end
+  end
+
+  def flash_message(conn, %{"error" => message}) do
+    if message do
+      render "flash.html", class: "alert-danger", message: message
+    end
+  end
+
+  def flash_message(_), do: nil
 end
