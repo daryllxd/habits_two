9622b8fbb4ab8dd28ca233e86af910ab6be5dd46
Daryll Santos <daryll.santos@gmail.com>
2018-03-11 19:25:19 +0800

    When migrating, also include dump task.

--- mix.exs
+++ mix.exs
@@ -57,6 +57,7 @@ defmodule HabitsTwo.Mixfile do
   defp aliases do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.migrate": ["ecto.migrate", "ecto.dump"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
